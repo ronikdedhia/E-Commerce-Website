23026f9f1528587acfb0f02950a7e382e5c358df
ronikdedhia <ronikdedhia@gmail.com>
2022-04-22 00:41:53 +0530

    initial commit

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer" style="background-color: darkgray">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Brought to you by the entire team of WBMart</strong>
+    </div>
+</footer>
